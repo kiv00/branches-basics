18625bb5a859863f4dd1f6978ba305ff60c41d1e
Корхов Иван Владимирович <ip915s13@awp01.csc.local>
2020-03-16 12:03:00 +0700

    after scan x decreases by 1

--- main.c
+++ main.c
@@ -2,6 +2,11 @@ include <stdio.h>
 include <stdlib.h>
 
 int main(){
- 	printf("branches-bascis\n");
+	int x;
+	scanf("%d",&x);
+	x--;
+	if(x>5){
+ 		printf("branches-bascis\n %d\n",x);
+	} else printf("x<5\n");
 	return 1;
 }
